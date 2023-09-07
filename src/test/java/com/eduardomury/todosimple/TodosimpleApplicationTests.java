4f85c6383f68da0700d7160e6154b1889206ad48
Eduardo Mury Assafin <101987520+Dudzz01@users.noreply.github.com>
2023-09-07 16:30:36 -0300

    Spring Boot Project

---
+++ src/test/java/com/eduardomury/todosimple/TodosimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardomury.todosimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
